84828307b2da4ad7849f6d0a8433fe35ae15af8e
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-16 23:33:48 +0600

    header section added

---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../images/Logo-Comgo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="header-bg py-3 sticky-top"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand as={Link} to="/home">
+          <img className="img-fluid" width="170px" src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/home">
+              Homepage
+            </Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#pricing">Visa</Nav.Link>
+            <Nav.Link href="#pricing">Coaching</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Visa</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
