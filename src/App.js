ffebb85738320d336188761f8316d01a11e2241f
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-17 18:01:05 +0600

    about and blogs section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from "./Page/HomePage/HomePage";
 import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
 import PageNotFound from "./Shared/NotFoundPage/PageNotFound";
+import About from "./Page/About/About";
+import Blogs from "./Page/Blogs/Blogs";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
---
+++ src/Page/Blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blogs = () => {
+  return (
+    <div>
+      <h3>This Is Blogs Section</h3>
+    </div>
+  );
+};
+
+export default Blogs;
