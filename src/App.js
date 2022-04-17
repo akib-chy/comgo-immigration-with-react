cde2ac835b658a77e30c14fc0ab8ffa4673c40f7
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-17 16:44:10 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Page/HomePage/HomePage";
+import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,10 +1,105 @@
 import React from "react";
+import "./Footer.css";
+import logo from "../../images/Logo-Comgo.png";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>this is footer section</h2>
-    </div>
+    <footer className="footer-section pt-5 pb-5">
+      <div className="container  pt-5  pb-5">
+        <div className="text-center">
+          <img className="img-fluid" src={logo} alt="" />
+          <div className="text-light">
+            <p className="mb-1 mt-4">
+              Duis dictum vestibulum ante vitae ullamcorper. Phasellus
+            </p>
+            <p>ullamcorper odio vitae eleifend ultricies lectus</p>
+            <div className="footer-icons mt-5 ">
+              <a href="#">
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-youtube"></i>
+              </a>
+              <a href="#">
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div className="my-5">
+            <hr className="text-light" />
+          </div>
+        </div>
+        <div className="row footer-info">
+          <div className="col-lg-2">
+            <h4 className="text-light fw-bold">Services</h4>
+            <div className="mt-3">
+              <a href="#">Worker Visa</a>
+            </div>
+            <div className="mt-3">
+              <a href="#">Worker Visa</a>
+            </div>
+            <div className="mt-3">
+              <a href="#">Worker Visa</a>
+            </div>
+            <div className="mt-3">
+              <a href="#">Worker Visa</a>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <h4 className="text-light fw-bold">Important Links</h4>
+            <div className="mt-3">
+              <a href="#">About</a>
+            </div>
+            <div className="mt-3">
+              <a href="#">Coaching</a>
+            </div>
+            <div className="mt-3">
+              <a href="#">Visa</a>
+            </div>
+            <div className="mt-3">
+              <a href="#">Pricing</a>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <h4 className="text-light fw-bold">Contact</h4>
+            <div className="text-light d-flex align-items-center">
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+              </span>
+              <p>Marolien Street, no 14, 2nd floor</p>
+            </div>
+            <div className="text-light d-flex align-items-center">
+              <span>
+                <i className="fa-solid fa-phone"></i>
+              </span>
+              <p>+62 820 1212 5050</p>
+            </div>
+            <div className="text-light d-flex align-items-center">
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+              </span>
+              <p>tokomoo@example.com</p>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <h4 className="text-light fw-bold">Subscribe Our Newslatter</h4>
+            <div>
+              <p className="mb-1">Lorem ipsum dolor sit amet,</p>
+              <p>consectetur adipiscing elit. </p>
+              <div className="footer-form">
+                <input type="email" placeholder="Your Email" />
+                <button className="btn shadow-none" type="submit">
+                  <i className="fa-solid fa-angle-right ms-1 mt-2"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
